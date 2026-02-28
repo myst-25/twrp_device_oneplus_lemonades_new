41b0685c2597b8d81bd618ffc843605d43c93f8c
myst-25 <mohammedyaminsalman@gamil.com>
2026-02-28 05:44:50 +0530

    Initial TWRP device tree for OnePlus 9R

---
+++ twrp_lemonades.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from lemonades device
+$(call inherit-product, device/oneplus/lemonades/device.mk)
+
+PRODUCT_DEVICE := lemonades
+PRODUCT_NAME := twrp_lemonades
+PRODUCT_BRAND := oneplus
+PRODUCT_MODEL := LE2101
+PRODUCT_MANUFACTURER := oneplus
